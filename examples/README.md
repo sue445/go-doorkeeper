f746f56d05c16467a1ba65193f5ffea471c227fd
sue445 <sue445@sue445.net>
2019-07-31 14:10:25 +0900

    Add examples

---
+++ examples/README.md
@@ -0,0 +1,10 @@
+# Examples
+## Usage
+```bash
+export DOORKEEPER_ACCESS_TOKEN=xxxxxxxx
+
+go run get_event.go
+go run get_events.go
+go run get_group.go
+go run get_group_events.go
+```
---
+++ examples/get_events.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sue445/go-doorkeeper"
+	"os"
+)
+
+func main() {
+	accessToken := os.Getenv("DOORKEEPER_ACCESS_TOKEN")
+	if accessToken == "" {
+		panic("DOORKEEPER_ACCESS_TOKEN is required")
+	}
+
+	client := doorkeeper.NewClient(accessToken)
+	events, rateLimit, err := client.GetEvents(&doorkeeper.GetEventsParams{})
+
+	if err != nil {
+		panic(fmt.Sprintf("err=%+v", err))
+	}
+
+	for i, event := range events {
+		fmt.Printf("event[%d]=%+v\n", i, event)
+	}
+	fmt.Printf("rateLimit=%+v\n", rateLimit)
+}
